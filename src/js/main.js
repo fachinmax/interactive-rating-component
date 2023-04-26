cd48e49fa16e08e102296c30c120ed4b460fb290
fachinmax <maxfachin98@gmail.com>
2023-04-26 20:11:48 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// functions
+const showResult = show => {
+    console.log('ok');
+    if(show) {
+        resultSection.style.display = 'flex';
+        choiceSection.style.display = 'none';
+    } else {
+        resultSection.style.display = 'none';
+        choiceSection.style.display = 'block';
+    }
+    
+}
+
+const setRate = number => {
+    let ratingElement = document.querySelector('span');
+    ratingElement.innerHTML = number;
+}
+
+// take all elements
+let resultSection = document.querySelector('.result');
+let choiceSection = document.querySelector('.choices');
+
+// set elements
+showResult(false);
+resultSection.addEventListener("click", () => {
+    showResult(false);
+});
